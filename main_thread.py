ec3fe280a535b4eefe0112d5f593b4a0c532e268
DiscoFT <84707219+DiscoFT@users.noreply.github.com>
2021-05-25 22:04:47 +0100

    Add files via upload

--- main_thread.py
+++ main_thread.py
@@ -1,3 +1,19 @@
+""" DiscoFT is a CSV chat analyser and Report maker
+    Copyright (C) 2021  Oliver Greenwood
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+
 import sys, os # Running OS functions
 from PyQt5.QtWidgets import * # GUI
 from PyQt5.QtGui import * # GUI
@@ -12,7 +28,7 @@ import collections # Tabulation
 import re # Regular Expressions
 from wordcloud import WordCloud # Required for WordMap
 import ntpath # Pathing (Windows)
-import en_core_web_sm # Required but not featured as a standalone module.
+import en_core_web_sm # Required from Spacy but not featured as a standalone module.
 import gensim # Topic Modelling
 import gensim.corpora as corpora # Topic Modelling
 import pyLDAvis # LDA Visualisation
